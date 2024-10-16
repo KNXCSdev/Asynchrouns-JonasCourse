4a54247e6e2e15bf639c560c642e3d9cc52ace58
KNXCSdev <michal.miksho@gmail.com>
2024-10-16 14:42:46 +0200

    Fetch API,consuming,chaining

--- script.js
+++ script.js
@@ -6,55 +6,81 @@ const countriesContainer = document.querySelector('.countries');
 ///////////////////////////////////////
 
 const renderCountry = function (data, className = '') {
-  const languages = Object.values(data.languages)[0];
-  const currencies = Object.values(data.currencies)[0].name;
+  const name = data.name.common;
+  const flag = data.flags.svg;
+  const region = data.region;
+  const language = Object.values(data.languages)[0];
+  const currency = Object.values(data.currencies)[0].name;
 
-  const html = ` 
-        <article class="country ${className}">
-          <img class="country__img" src="${data.flags.svg}" />
-          <div class="country__data">
-            <h3 class="country__name">${data.name.common}</h3>
-            <h4 class="country__region">${data.region}</h4>
-            <p class="country__row"><span>👫</span>${(
-              +data.population / 1000000
-            ).toFixed(2)} people</p>
-            <p class="country__row"><span>🗣️</span>${languages}</p>
-            <p class="country__row"><span>💰</span>${currencies}</p>
-          </div>
-        </article>`;
-
+  const html = `
+  <article class="country ${className}">
+    <img class="country__img" src="${flag}" />
+    <div class="country__data">
+      <h3 class="country__name">${name}</h3>
+      <h4 class="country__region">${region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 1000000
+      ).toFixed(1)} people</p>
+      <p class="country__row"><span>🗣️</span>${language}</p>
+      <p class="country__row"><span>💰</span>${currency}</p>
+    </div>
+  </article>
+  `;
   countriesContainer.insertAdjacentHTML('beforeend', html);
   countriesContainer.style.opacity = 1;
 };
 
-const getCountryAndNeighbour = function (country) {
-  //ajax call country 1
-  const request = new XMLHttpRequest();
-  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
-  request.send();
+// const getCountryAndNeighbour = function (country) {
+//   //ajax call country 1
+//   const request = new XMLHttpRequest();
+//   request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+//   request.send();
 
-  request.addEventListener('load', function () {
-    const [data] = JSON.parse(this.responseText);
+//   request.addEventListener('load', function () {
+//     const [data] = JSON.parse(this.responseText);
 
-    //RENDER COUNTRY 1
-    renderCountry(data);
+//     //RENDER COUNTRY 1
+//     renderCountry(data);
 
-    //Get neighbour conuntry
-    const [neighbour] = data.borders;
+//     //Get neighbour conuntry
+//     const [neighbour] = data.borders;
 
-    if (!neighbour) return;
+//     if (!neighbour) return;
 
-    //AJAX call country 2
-    const request2 = new XMLHttpRequest();
-    request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour}`);
-    request2.send();
+//     //AJAX call country 2
+//     const request2 = new XMLHttpRequest();
+//     request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour}`);
+//     request2.send();
 
-    request2.addEventListener('load', function () {
-      const [data2] = JSON.parse(this.responseText);
+//     request2.addEventListener('load', function () {
+//       const [data2] = JSON.parse(this.responseText);
 
-      renderCountry(data2, 'neighbour');
+//       renderCountry(data2, 'neighbour');
+//     });
+//   });
+// };
+
+// getCountryAndNeighbour('germany');
+
+const getCountryData = function (country) {
+  //Country 1
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then(response => response.json())
+    .then(data => {
+      renderCountry(data[0]);
+
+      const neighbour = data[0]?.borders[0];
+
+      if (!neighbour) return;
+
+      //Country 2
+      return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      renderCountry(data[0], 'neighbour');
     });
-  });
 };
 
-getCountryAndNeighbour('germany');
+getCountryData('poland');
